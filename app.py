c872b78689c3078cd822cdb0a148afeb06370b13
Nicolas Caseiro de Abreu <nicolascaseirodeabreu@gmail.com>
2025-12-31 12:35:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -35,12 +35,13 @@ def ordenar_meses(meses_selecionados):
     return sorted(meses_selecionados, key=lambda m: meses_pt.index(m))
 
 def ordenar_pt(series):
-    return pd.Series(series).dropna().sort_values(
-        key=lambda s: s.str.normalize('NFKD')
-                       .str.encode('ascii', errors='ignore')
-                       .str.decode('utf-8')
-                       .str.lower()
-    ).unique().tolist()
+    s = pd.Series(series).dropna().unique()
+    return pd.Series(s).sort_values(
+        key=lambda x: x.str.normalize('NFKD')
+                        .str.encode('ascii', errors='ignore')
+                        .str.decode('utf-8')
+                        .str.lower()
+    ).tolist()
 
 meses_disponiveis = ordenar_meses(df_temp['Mês'].dropna().unique())
 generos_disponiveis = sorted(df_temp['Gêneros_lista'].dropna().unique())
